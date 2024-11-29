6736ad87aad999f34745c842c5d635486ae9934e
Zaine <kyawkokozin223@gmail.com>
2024-11-29 16:23:55 +0630

    output list of students

--- src/main/java/com/zainecode/SpringCrudApplication.java
+++ src/main/java/com/zainecode/SpringCrudApplication.java
@@ -1,13 +1,33 @@
 package com.zainecode;
 
+import com.zainecode.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCrudApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> sayHello(){
+		return List.of(
+				new Student(
+                                    1L,
+                                    "Kyaw Ko Ko Zin",
+                                    "Kyawkokozin223@gmail.com",
+                        LocalDate.of(2001, Month.JANUARY,8),
+                        25)
+		);
 	}
 
 }
